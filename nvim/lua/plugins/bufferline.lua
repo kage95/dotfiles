2ada5d4caa97d7df83d8f97f347be645b16295c2
kage95 <81172243+kage95@users.noreply.github.com>
2024-09-26 17:44:08 +0900

    update

--- nvim/lua/plugins/bufferline.lua
+++ nvim/lua/plugins/bufferline.lua
@@ -4,53 +4,6 @@ return {
     options = {
       show_buffer_close_icons = false,
       show_close_icon = false,
-      truncate_names = false,
-      separator_style = "slant",
-    },
-    highlights = {
-      buffer_selected = {
-        fg = "#ffffff",
-        bg = "#333366",
-      },
-      separator_selected = {
-        bg = "#333366",
-      },
-      indicator_selected = {
-        bg = "#333366",
-      },
-      modified_selected = {
-        bg = "#333366",
-      },
-      error_selected = {
-        bg = "#333366",
-      },
-      info_selected = {
-        bg = "#333366",
-      },
-      hint_selected = {
-        bg = "#333366",
-      },
-      warning_selected = {
-        bg = "#333366",
-      },
-      diagnostic_selected = {
-        bg = "#333366",
-      },
-      warning_diagnostic_selected = {
-        bg = "#333366",
-      },
-      error_diagnostic_selected = {
-        bg = "#333366",
-      },
-      info_diagnostic_selected = {
-        bg = "#333366",
-      },
-      hint_diagnostic_selected = {
-        bg = "#333366",
-      },
-      background = {
-        fg = "#ffffff",
-      },
     },
   },
 }
--- nvim/lua/plugins/indent-blankline.lua
+++ nvim/lua/plugins/indent-blankline.lua
@@ -10,7 +10,7 @@ return {
         remove_blankline_trail = true,
       },
       scope = {
-        enabled = true,
+        enabled = false,
       },
     })
   end,
