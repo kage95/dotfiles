80e7f9e9fdf5f34b0fcff8d6339724449a60a7e2
kage95 <81172243+kage95@users.noreply.github.com>
2024-12-14 17:28:14 +0900

    update goto-preview

--- nvim/lua/plugins/goto-preview.lua
+++ nvim/lua/plugins/goto-preview.lua
@@ -1,17 +1,23 @@
 return {
   "rmagatti/goto-preview",
-  event = "BufEnter",
+  -- event = "BufEnter",
   config = function()
     vim.keymap.set("n", "gp", "<cmd>lua require('goto-preview').goto_preview_definition()<CR>", { noremap = true })
 
     require("goto-preview").setup({
-      post_open_hook = function(buf)
+      post_open_hook = function(buf, win)
         vim.keymap.set("n", "q", "<cmd>lua require('goto-preview').close_all_win()<CR>", { buffer = buf })
+
+        vim.api.nvim_set_option_value("number", false, { win = win })
+        vim.api.nvim_set_option_value("statuscolumn", "", { win = win })
+        vim.api.nvim_set_option_value("signcolumn", "no", { win = win })
       end,
-      post_close_hook = function()
-        local buffer = vim.api.nvim_get_current_buf()
-        vim.api.nvim_buf_del_keymap(buffer, "n", "q")
+      post_close_hook = function(buf)
+        vim.api.nvim_buf_del_keymap(buf, "n", "q")
       end,
+      preview_window_title = { enable = true, position = "left" },
+      border = { "╭", "─", "╮", "│", "╯", "─", "╰", "│" },
+      width = 80,
     })
   end,
 }
