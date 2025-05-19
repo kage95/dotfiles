d1a678f08e2035dd556438911f57239cee9d9cc5
kage95 <81172243+kage95@users.noreply.github.com>
2025-05-19 22:44:25 +0900

    chore

--- nvim/lua/plugins/disabled.lua
+++ nvim/lua/plugins/disabled.lua
@@ -1,6 +1,4 @@
 return {
   { "folke/flash.nvim", enabled = false },
-  { "lukas-reineke/indent-blankline.nvim", enabled = false },
-  { "ibhagwan/fzf-lua", enabled = false },
   { "nvim-neo-tree/neo-tree.nvim", enabled = false },
 }
