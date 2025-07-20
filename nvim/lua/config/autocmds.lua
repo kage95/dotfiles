f9dad821082bb507c93c42986cf5e75c168bfd15
kage95 <81172243+kage95@users.noreply.github.com>
2025-07-20 11:42:06 +0900

    chore

--- nvim/lua/config/autocmds.lua
+++ nvim/lua/config/autocmds.lua
@@ -13,12 +13,3 @@ vim.api.nvim_create_autocmd("FileType", {
     vim.opt_local.spell = false
   end,
 })
-
--- Goのインデント幅を設定
-vim.api.nvim_create_autocmd("FileType", {
-  pattern = "go",
-  callback = function()
-    vim.opt_local.tabstop = 4
-    vim.opt_local.shiftwidth = 4
-  end,
-})
